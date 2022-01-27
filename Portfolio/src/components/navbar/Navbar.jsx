ba08ef492ee26c156718662c2135cc7730e04e49
AlbertoCanales1 <albertocanales1@outlook.com>
2022-01-27 00:33:56 -0600

    Finalized nav structure

--- Portfolio/src/components/navbar/Navbar.jsx
+++ Portfolio/src/components/navbar/Navbar.jsx
@@ -2,8 +2,20 @@ import "./navbar.scss"
 
 export default function Navbar() {
     return (
-        <div className ="navbar">
-            
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className ="logo">Alberto Canales</a>
+                <div className= "itemContainer">
+                    
+                </div>
+                </div>
+
+                <div className="right">
+
+                </div>
+
+            </div>
         </div>
     )
 }
