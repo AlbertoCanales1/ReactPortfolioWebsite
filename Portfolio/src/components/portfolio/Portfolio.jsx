925ad184cbc60a25d0c61e8f92f0e52102866bca
AlbertoCanales1 <albertocanales1@outlook.com>
2022-02-07 02:00:16 -0600

    Finalized works component

--- Portfolio/src/components/portfolio/Portfolio.jsx
+++ Portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("resume")
+    const list = [
+    {
+      id: "resume",
+      title: "Resume",
+    },
+    {
+        id: "certifications",
+        title: "Certifications ",
+    },
+  
+    ];
     return (
         <div className ="portfolio" id ="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+             {list.map(item => (
+                 <PortfolioList title={item.title} active={selected === item.id}
+                  setSelected={setSelected}
+                  id={item.id}
+                  />
+             ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img
+                    src="https://icon-library.com/images/calendar-of-events-icon/calendar-of-events-icon-11.jpg" 
+                    alt=""
+                     />
+                    <h3>App</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- Portfolio/src/components/works/Works.jsx
+++ Portfolio/src/components/works/Works.jsx
@@ -1,9 +1,69 @@
+import { useState } from "react"
 import "./works.scss"
 
 export default function Works() {
+    const [currentSlide,setCurrentSlide] =useState(0)
+
+
+    const data = [
+    {
+        id: "1",
+        icon: "assets/schedule.png",
+        title: "Event Booker",
+        desc: "An   application that helps you book events based on your zip code",
+        img: "https://www.ualberta.ca/information-services-and-technology/media-library/images/tl-schedule.jpg",
+       
+    },
+    {
+        id: "2",
+        icon: "assets/recipe.png",
+        title: "Recipe Finder",
+        desc: "An application that allows you to browse your favorite recipes and even add some of your own",
+        img: "https://www.freepnglogos.com/uploads/food-png/food-harrison-catering-contract-caterers-serving-top-quality-32.png",
+        
+    },
+]
+
+
+    const handleClick =(way) =>{
+        way === "left" ? setCurrentSlide(currentSlide > 0 ?currentSlide -1: 1) : 
+        // setCurrentSlide(currentSlide<data.length -1)
+        setCurrentSlide(currentSlide<data.length -1?currentSlide+1 : 0)
+    }
     return (
         <div className ="works" id ="works">
+
             
+            
+            <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
+                {data.map(d=>(
+
+                
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={d.icon} alt=""/>
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>
+                                    {d.desc}
+                                </p>
+                                {/* <span>Projects</span> */}
+                            </div>
+                        </div>
+                        <div className="right">
+                            
+                            <img src={d.img}alt="" />
+                        </div>
+                    </div>
+                </div>
+                ))}
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" onClick={()=>handleClick("left")}/>
+            <img src="assets/arrow.png" className="arrow right" alt="" onClick={()=>handleClick("right")} />
+
         </div>
     )
 }
