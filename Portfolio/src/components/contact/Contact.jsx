b2ef33c36f9f3613783d661be1c886ccf19bc96c
AlbertoCanales1 <albertocanales1@outlook.com>
2022-02-10 01:23:39 -0600

    finalized contact component

--- Portfolio/src/components/contact/Contact.jsx
+++ Portfolio/src/components/contact/Contact.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import "./contact.scss"
+import emailjs from "emailjs-com"
 
 export default function Contact() {
+
+
+
 
 const [message, setMessage] = useState(false)
 
 const handleSubmit = (e)=>{
     e.preventDefault();
     setMessage(true)
+
+    emailjs.sendForm('gmail', 'template_4vah1pz', e.target, 
+    'user_JLyiSdvukmqlIHoXBjoPg')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset()
+
+
 }
 
     return (
@@ -16,10 +31,11 @@ const handleSubmit = (e)=>{
                 <img src="assets/shake2.png" alt="" />
             </div>
             <div className="right">
-                <h2>Contact.</h2>
+                <h2>Contact Me.</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="Email" />
-                    <textarea placeholder="Message"></textarea>
+                    <input type="email" placeholder="email" name="email"/>
+                    <input type="subject" placeholder="subject" name="subject"/>
+                    <textarea placeholder="message" name="message"></textarea>
                     <button type="submit">Send</button>
                     {message && <span> Thank you. I will respond promptly.</span>}
                 </form>
